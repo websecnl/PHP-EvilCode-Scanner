519664d650ef991ac50dab50f242c616814cd6b3
Joel Aviad Ossi <rebitclown105@hotmail.com>
2017-11-07 14:09:42 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,28 +3,29 @@
 # Run this script inside the domain directory
 # Usage: ./scan.sh
 # -----------------------------------------------------------------------------
-workdir = /var/www/html
+workdir='/var/www/html'
+logfile='security.txt'
 
-find $workdir -type f -exec grep 'eval(' {} \; -exec echo '[PHP CODE EXECUTION] FOUND EVAL:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'eval(' {} \; -exec echo '[PHP CODE EXECUTION] FOUND EVAL:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] eval SCAN COMPLETE';
-find $workdir -type f -exec grep 'preg_replace(' {} \; -exec echo '[PHP CODE EXECUTION] FOUND PREG_REPLACE:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'preg_replace(' {} \; -exec echo '[PHP CODE EXECUTION] FOUND PREG_REPLACE:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] preg_replace SCAN COMPLETE';
-find $workdir -type f -exec grep 'assert(' {} \; -exec echo '[PHP CODE EXECUTION] FOUND ASSERT:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'assert(' {} \; -exec echo '[PHP CODE EXECUTION] FOUND ASSERT:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+]  SCAN COMPLETE';
-find $workdir -type f -exec grep '``' {} \; -exec echo '[COMMAND EXECUTION] FOUND backticks:' {} >> security_log.txt \;
+find $workdir -type f -exec grep '``' {} \; -exec echo '[COMMAND EXECUTION] FOUND backticks:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] backticks SCAN COMPLETE';
-find $workdir -type f -exec grep 'exec(' {} \; -exec echo '[COMMAND EXECUTION] FOUND EXEC:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'exec(' {} \; -exec echo '[COMMAND EXECUTION] FOUND EXEC:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] exec SCAN COMPLETE';
-find $workdir -type f -exec grep 'popen(' {} \; -exec echo '[COMMAND EXECUTION] FOUND POPEN:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'popen(' {} \; -exec echo '[COMMAND EXECUTION] FOUND POPEN:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] popen SCAN COMPLETE';
-find $workdir -type f -exec grep 'proc_open(' {} \; -exec echo '[COMMAND EXECUTION] FOUND PROC_OPEN:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'proc_open(' {} \; -exec echo '[COMMAND EXECUTION] FOUND PROC_OPEN:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] proc_open SCAN COMPLETE';
-find $workdir -type f -exec grep 'pcntl_exec(' {} \; -exec echo '[COMMAND EXECUTION] FOUND PCNTL_EXEC:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'pcntl_exec(' {} \; -exec echo '[COMMAND EXECUTION] FOUND PCNTL_EXEC:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] pcntl_exec SCAN COMPLETE';
-find $workdir -type f -exec grep 'system(' {} \; -exec echo '[COMMAND EXECUTION] FOUND SYSTEM:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'system(' {} \; -exec echo '[COMMAND EXECUTION] FOUND SYSTEM:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] system SCAN COMPLETE';
-find $workdir -type f -exec grep 'shell_exec(' {} \; -exec echo '[COMMAND EXECUTION] FOUND SHELL_EXEC:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'shell_exec(' {} \; -exec echo '[COMMAND EXECUTION] FOUND SHELL_EXEC:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] shell_exec SCAN COMPLETE';
-find $workdir -type f -exec grep 'passthru(' {} \; -exec echo '[COMMAND EXECUTION] FOUND PASSTHRU:' {} >> security_log.txt \;
+find $workdir -type f -exec grep 'passthru(' {} \; -exec echo '[COMMAND EXECUTION] FOUND PASSTHRU:' {} -Eron  r"%}(.*){%" >> logfile \;
 echo '[+] passthru SCAN COMPLETE';
-echo '[+] output saved to security_log.txt';
+echo '[+] output saved';
